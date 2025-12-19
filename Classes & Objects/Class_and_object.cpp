266780ca4cd6ff25f99bcb6de381db8a8e09144f
Nafiz144 <nafizahamed0464@gmail.com>
2025-12-19 20:16:56 +0600

    19

---
+++ Classes & Objects/Class_and_object.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class employee
+{
+public:
+    string name;
+    string gender;
+    int age;
+
+    void introduction()
+    {
+
+        cout << "Employee Name - " << name << endl;
+        cout << "Employee Gender - " << gender << endl;
+        cout << "Employee Age - " << age << endl;
+    }
+
+    employee(string name, string gender, int age)
+    {
+        this->name = name;
+        this->gender = gender;
+        this->age = age;
+    };
+};
+
+int main()
+{
+
+    employee e1 = employee("nafiz", "male", 25);
+    e1.introduction();
+
+    employee e2 = employee("saldina", "female", 25);
+    e2.introduction();
+}
